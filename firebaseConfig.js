a579f381fa4eb57227feb8ea9893f14eab3a94cf
Sania029 <122147510+Sania029@users.noreply.github.com>
2025-05-02 22:53:15 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCCoy-otAepS4fzRpQzWnfb0bjHr6Momqo",
-    authDomain: "enableauthentication-3742c.firebaseapp.com",
-    projectId: "enableauthentication-3742c",
-    storageBucket: "enableauthentication-3742c.firebasestorage.app",
-    messagingSenderId: "923457480531",
-    appId: "1:923457480531:web:8f766c2970264aa7f4e3a6",
-    measurementId: "G-CQ86HTHZ8W"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
